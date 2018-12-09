50ae47ee6ad2299b73db255833b6cce577931936
WXL <wangxl1029@sina.com>
2018-12-09 17:47:31 +0800

    Refactor: Active/passive message worker. 1) The active message worker out message per second. 2) The passive message worker includes a controller and a worker.    - The controller is QT event loop thread and respond signal.    - The worker defines the signals/slots to answer and respond.

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 import sys
 from PyQt5.QtWidgets import (QWidget, QHBoxLayout, QSplitter, QListWidget, QApplication, QTextEdit)
-from PyQt5.QtCore import Qt, pyqtSignal, QObject
+from PyQt5.QtCore import Qt, pyqtSignal, QObject, QThread
 from PyQt5.QtGui import QTextCursor
 
 from alive import MessageMemory
 
 
-class FileSystemSensor:
-    pass
+class MessageActiveThread(QThread):
+    sig_output = pyqtSignal(str)
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def run(self):
+        i = 1
+        while True:
+            self.sleep(1)
+            self.sig_output.emit(f'#{i} some inner message\n')
+            i = i + 1
+
+
+class MessageWorker(QObject):
+    work_done = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+    def do_work(self, msg):
+        self.work_done.emit(f"{msg} done.\n")
+
+
+class MessageController(QObject):
+    operate = pyqtSignal(str)
+    operate_done = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.worker_thread = QThread()
+        self.worker = MessageWorker()
+        self.worker.moveToThread(self.worker_thread)
+        self.operate.connect(self.worker.do_work)
+        self.worker.work_done.connect(self.operate_done)
+        self.worker_thread.start()
+
+    def __del__(self):
+        self.worker_thread.quit()
+        self.worker_thread.wait()
 
 
 class MessageReceiver(QObject):
@@ -51,7 +89,9 @@ class ChatUI(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.msg_receiver = MessageReceiver()
+        #self.msg_receiver = MessageReceiver()
+        self.controller = MessageController()
+        self.active_msg = MessageActiveThread(self)
         self.init_ui()
 
     def init_ui(self):
@@ -65,7 +105,8 @@ class ChatUI(QWidget):
         input_edit = InputEdit(self)
         input_edit.setMinimumHeight(50)
         input_edit.sig_input.connect(output_edit.message)
-        input_edit.sig_input.connect(self.msg_receiver.on_input)
+        #input_edit.sig_input.connect(self.msg_receiver.on_input)
+        input_edit.sig_input.connect(self.controller.operate)
         # the right panel
         chat_list = QListWidget(self)
 
@@ -85,8 +126,13 @@ class ChatUI(QWidget):
 
         # the main window
         self.setGeometry(300, 300, 600, 400)
-        self.setWindowTitle('Ape chatting')
+        self.setWindowTitle('My chat')
         self.show()
+
+        # active/passive threads
+        self.controller.operate_done.connect(output_edit.message)
+        self.active_msg.sig_output.connect(output_edit.message)
+        self.active_msg.start()
 
 
 if __name__ == '__main__':
