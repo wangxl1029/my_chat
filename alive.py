fc0a220212b332cc34e72403633c784d390bc383
WXL <wangxl1029@sina.com>
2018-12-17 19:04:23 +0800

    Refactor savepoint @ not test, to be continued : move the class of alive memory to standalone module.

--- alive.py
+++ alive.py
@@ -23,16 +23,6 @@ class FsCommandEnum(Enum):
     walk = 4
 
 
-@unique
-class MemoryInfoEnum(Enum):
-    fs_target_reset_done = 0
-    fs_target_prop_done = 1
-    fs_target_list_done = 2
-    fs_target_walk_done = 3
-
-    msg_input = 10
-
-
 def reported(function):
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
@@ -81,38 +71,6 @@ class QueuePipe:
         self.__oq.join()
 
 
-class AliveMemory(AliveThread):
-    def __init__(self):
-        super().__init__()
-        self.__qp = QueuePipe()
-
-    def __del__(self):
-        self.__qp.task_done()
-        self.__qp.join()
-
-    # override the method of supper class
-    def run(self):
-        while True:
-            try:
-                info_type, info_data = self.__qp.inner_get(True, 1)
-                if info_type == MemoryInfoEnum.msg_input:
-                    self.__qp.inner_put(info_data)
-                print('memory alive!')
-            except queue.Empty:
-                print('memory timeout')
-
-            cmd = random.choice([FsCommandEnum.reset, FsCommandEnum.get_prop,
-                                 FsCommandEnum.list_dir, FsCommandEnum.walk])
-            g_fs_sensor.send_cmd(cmd)
-
-    def put(self, item):
-        self.__qp.outer_put(item)
-
-    def get(self):
-        return self.__qp.outer_get()
-
-
-g_mem = AliveMemory.create()
 
 
 class FilesystemSensor(AliveThread):
---
+++ alive_mem.py
@@ -0,0 +1,50 @@
+import queue
+from enum import Enum, unique
+
+import random
+
+from alive import *
+
+
+@unique
+class MemoryInfoEnum(Enum):
+    fs_target_reset_done = 0
+    fs_target_prop_done = 1
+    fs_target_list_done = 2
+    fs_target_walk_done = 3
+
+    msg_input = 10
+
+
+class AliveMemory(AliveThread):
+    def __init__(self):
+        super().__init__()
+        self.__qp = QueuePipe()
+
+    def __del__(self):
+        self.__qp.task_done()
+        self.__qp.join()
+
+    # override the method of supper class
+    def run(self):
+        while True:
+            try:
+                info_type, info_data = self.__qp.inner_get(True, 1)
+                if info_type == MemoryInfoEnum.msg_input:
+                    self.__qp.inner_put(info_data)
+                print('memory alive!')
+            except queue.Empty:
+                print('memory timeout')
+
+            cmd = random.choice([FsCommandEnum.reset, FsCommandEnum.get_prop,
+                                 FsCommandEnum.list_dir, FsCommandEnum.walk])
+            g_fs_sensor.send_cmd(cmd)
+
+    def put(self, item):
+        self.__qp.outer_put(item)
+
+    def get(self):
+        return self.__qp.outer_get()
+
+
+g_mem = AliveMemory.create()
